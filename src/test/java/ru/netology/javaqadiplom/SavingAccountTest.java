92a7f732573949eba29fe1917d4751f650bcde2f
LeM <mihail.lebedev88@gmail.com>
2023-05-09 16:09:35 +0600

    add test for equals for max balance

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -18,6 +18,19 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+    @Test
+    public void shouldAddEqualsThanMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(8_000);
+
+        Assertions.assertEquals(2_000 + 8_000, account.getBalance());
+    }
 
     @Test
     public void shouldPositivePay() {
