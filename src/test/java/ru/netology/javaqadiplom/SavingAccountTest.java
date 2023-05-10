9819b1380ca07ee119c39733595f6879aae748ad
cistercii91 <103969442+cistercii91@users.noreply.github.com>
2023-05-10 15:37:57 +0300

    добавлен тест
    добавили удаленный по ошибке тест

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -6,6 +6,20 @@ import org.junit.jupiter.api.Test;
 public class SavingAccountTest {
     
     @Test
+    public void shouldAddLessThanMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+    }
+    
+    @Test
     public void shouldAddEqualsThanMaxBalance() {
         SavingAccount account = new SavingAccount(
                 3_000,
